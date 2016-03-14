7aa1f29d4ebb570fa59045a1eab255d628fcce88
Jian Huang <johnbodun@hotmail.com>
2016-03-14 02:42:16 -0400

    navigation drawer implemented

--- app/src/main/java/course1778/mobileapp/safeMedicare/Main/FamMemActivity.java
+++ app/src/main/java/course1778/mobileapp/safeMedicare/Main/FamMemActivity.java
@@ -1,19 +1,78 @@
 package course1778.mobileapp.safeMedicare.Main;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.parse.ParseUser;
+
+import course1778.mobileapp.safeMedicare.Authentication.LoginActivity;
 import course1778.mobileapp.safeMedicare.R;
 
 public class FamMemActivity extends AppCompatActivity {
     private static Context context;
+    private ListView drawerList;
+    private ArrayAdapter<String> drawerAdapter;
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
+    private String activityTitle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fam_mem_activity);
+
+        // action bar set up
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        activityTitle = getTitle().toString();
+
+        drawerList = (ListView)findViewById(R.id.navList);
+
+        // setup
+        addDrawerItems();
+        setupDrawer();
+
+
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+
+                    case 0:
+                        //Add medication
+                        break;
+
+                    case 1:
+                        // drag interaction
+                        break;
+
+                    case 2:
+                        // log out
+                        ParseUser.logOut();
+                        Intent intent = new Intent(getApplication(),
+                                LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
 
         FamMemFrag listFrag = new FamMemFrag();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -26,5 +85,63 @@ public class FamMemActivity extends AppCompatActivity {
     public static Context getContext()
     {
         return context;
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "Add Medication", "Drug Interactions", "Log Out", "Setting" };
+        drawerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        drawerList.setAdapter(drawerAdapter);
+    }
+
+    private void setupDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                null,
+                R.string.drawer_open,
+                R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(activityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.setDrawerListener(drawerToggle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Activate the navigation drawer toggle
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return (super.onOptionsItemSelected(item));
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    // handle configuration change such as portrait <--> landscape mode change
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
