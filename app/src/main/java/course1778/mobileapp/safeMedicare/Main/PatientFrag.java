915ddadeb6df1a245e5b91e69c75d4bf02efd647
Jian Huang <johnbodun@hotmail.com>
2016-03-26 21:45:05 -0400

    welcome page button improvements

--- app/src/main/java/course1778/mobileapp/safeMedicare/Main/PatientFrag.java
+++ app/src/main/java/course1778/mobileapp/safeMedicare/Main/PatientFrag.java
@@ -15,7 +15,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
-import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
